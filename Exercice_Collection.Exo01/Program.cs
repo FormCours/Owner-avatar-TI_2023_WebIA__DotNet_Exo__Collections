1d0f659d5e2331a9181e9e7e30f9fe029a3191e1
studentdot0500 <studentdot0500@TFNSDOT0500A>
2023-10-17 16:58:53 +0200

    Ajoutez des fichiers projet.

---
+++ Exercice_Collection.Exo01/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Obtenir les nombres premiers jusqu'à \n> ");
+int nbLimit = int.Parse(Console.ReadLine()!);
+
+List<int> nbPrimes =  new List<int>();
+int nbCountOp = 0;
+
+int nbCurrent = 2;
+while(nbCurrent <= nbLimit)
+{
+    bool isPrime = true;
+    int nbDiv = 2;
+
+    while (isPrime && nbDiv <= (nbCurrent/2))
+    {
+        nbCountOp++;
+        if (nbCurrent % nbDiv == 0)
+        {
+            isPrime = false;
+        }
+        nbDiv++;
+    }
+
+    if(isPrime)
+    {
+        nbPrimes.Add(nbCurrent);
+    }
+    nbCurrent++;
+}
+
+
+Console.WriteLine("Liste des nombres premiers : ");
+Console.WriteLine(string.Join(" > ", nbPrimes));
+
+Console.WriteLine($"Nb operation : {nbCountOp}");
---
+++ Exercice_Collection.Exo02/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Obtenir les X nombres premiers \n> ");
+int nbCount = int.Parse(Console.ReadLine()!);
+
+List<int> nbPrimes = new List<int>();
+int nbCountOp = 0;
+
+if(nbCount > 1)
+{
+    nbPrimes.Add(2);
+}
+
+for(int nbCurrent = 3; nbPrimes.Count < nbCount; nbCurrent += 2)
+{
+    bool isPrime = true;
+
+    for (int i = 0; isPrime && nbPrimes[i] < Math.Sqrt(nbCurrent) ; i++)
+    {
+        nbCountOp++;
+        if (nbCurrent % nbPrimes[i] == 0)
+        {
+            isPrime = false;
+        }
+    }
+
+    if(isPrime)
+    {
+        nbPrimes.Add(nbCurrent);
+    }
+}
+
+Console.WriteLine("Liste des nombres premiers : ");
+Console.WriteLine(string.Join(" > ", nbPrimes));
+
+Console.WriteLine($"Nb operation : {nbCountOp}");
